f049f64507ab39d4c41b3be30a7a399a182aaac9
Joseph Francis <hookedupjoe@users.noreply.github.com>
2024-12-19 12:13:07 -0500

    version bump

--- pressino-core.php
+++ pressino-core.php
@@ -27,9 +27,9 @@
  * Text Domain: actapp
  * License: GPLv3
  * 
- * Version: 2.0.2a
+ * Version: 2.0.3
  */
-define( 'PRESSINO_PLUGIN_CORE_VERSION', '2.0.2a' );
+define( 'PRESSINO_PLUGIN_CORE_VERSION', '2.0.3' );
 
 if ( !defined( 'ABSPATH' ) ) {
 	exit;
